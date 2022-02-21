5bacf526c3ef842f3e6f5a82ae026250e00e4fee
Danh Tran <danht185hcm@gmail.com>
2022-02-21 11:38:05 +0700

    feature [auth]: Add feature login

--- src/controllers/authCtrl.js
+++ src/controllers/authCtrl.js
@@ -41,6 +41,47 @@ class authCtrl {
       })
     }
   }
+
+  async login(req, res) {
+    const { username, password } = req.body
+    if (!username || !password)
+      return res.status(400).json({
+        success: false,
+        message: 'Please enter your username or password',
+      })
+
+    try {
+      const user = await User.findOne({ username })
+      if (!user)
+        return res.status(400).json({
+          success: false,
+          message: 'Incorrect username or password',
+        })
+
+      const passwordValid = await argon2.verify(user.password, password)
+      if (!passwordValid)
+        return res.status(400).json({
+          success: false,
+          message: 'Incorrect username or password',
+        })
+
+      const accessToken = jwt.sign(
+        { userId: user._id },
+        env.ACCESS_TOKEN_SECRET
+      )
+      return res.json({
+        success: true,
+        message: 'User logged in successfully',
+        accessToken,
+      })
+    } catch (e) {
+      console.log(e)
+      return res.status(500).json({
+        success: false,
+        message: 'Internal server error',
+      })
+    }
+  }
 }
 
 module.exports = new authCtrl()
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,18 @@ const express = require('express')
 const router = express.Router()
 const authCtrl = require('../controllers/authCtrl')
 
+/**
+ * @route POST api/auth/register
+ * @dec Register user
+ * @access public
+ */
 router.post('/register', authCtrl.register)
+
+/**
+ * @route POST api/auth/login
+ * @dec Login user
+ * @access public
+ */
+router.post('/login', authCtrl.login)
 
 module.exports = router
